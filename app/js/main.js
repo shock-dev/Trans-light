df36a8851a3ad072c0ef8c8c4a853d76e66fbe50
Andrey <shock.work@mail.ru>
2020-11-10 23:05:53 +0300

    rent section

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,12 @@ $(function(){
         nextArrow: '<button type="button" class="btn-reset slick-arrow slick-next"><svg width="12" height="19"><use href="img/sprite.svg#right-arrow"></use></svg></button>'
     })
 
+    $('.rent-slider').slick({
+        fade: true,
+        prevArrow: '<button type="button" class="btn-reset slick-arrow slick-prev"><svg width="12" height="19"><use href="img/sprite.svg#left-arrow"></use></svg></button>',
+        nextArrow: '<button type="button" class="btn-reset slick-arrow slick-next"><svg width="12" height="19"><use href="img/sprite.svg#right-arrow"></use></svg></button>'
+    })
+
     $('.tab').on('click', function (e) {
         e.preventDefault()
 
