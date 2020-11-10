e519580b988f85d16649f4ddc5fc7e7fc6e3d332
Andrey <shock.work@mail.ru>
2020-11-10 21:41:22 +0300

    catalog section

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,15 @@ $(function(){
         nextArrow: '<button type="button" class="btn-reset slick-arrow slick-next"><svg width="12" height="19"><use href="img/sprite.svg#right-arrow"></use></svg></button>'
     })
 
+    $('.tab').on('click', function (e) {
+        e.preventDefault()
+
+        $($(this).siblings()).removeClass('active')
+        $($(this).parent().siblings().find('div')).removeClass('active')
+
+        $(this).addClass('active')
+        $($(this).attr('href')).addClass('active')
+    })
+
 
 });
