20e40d5b27e89474af1d816f6b449df1f50f4a11
Andrey <shock.work@mail.ru>
2020-11-10 21:05:19 +0300

    added sprite.svg

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function(){
 
+    $('.projects-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        prevArrow: '<button type="button" class="btn-reset slick-arrow slick-prev"><svg width="12" height="19"><use href="img/sprite.svg#left-arrow"></use></svg></button>',
+        nextArrow: '<button type="button" class="btn-reset slick-arrow slick-next"><svg width="12" height="19"><use href="img/sprite.svg#right-arrow"></use></svg></button>'
+    })
 
 
-  
 });
